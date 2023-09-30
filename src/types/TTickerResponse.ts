7b7d709c8556299f264e4c5c6ba1487a0a9299d6
Szymon Wróbel <szym.wrobel@gmail.com>
2023-09-30 16:37:33 +0200

    feat: add request to zondacrypto api

---
+++ src/types/TTickerResponse.ts
@@ -0,0 +1,31 @@
+export type TTickerReponse =
+  | {
+      status: 'Ok';
+      ticker: {
+        market: {
+          code: string;
+          first: {
+            currency: string;
+            minOffer: string;
+            scale: number;
+          };
+          second: {
+            currency: string;
+            minOffer: string;
+            scale: number;
+          };
+          amountPrecision: number;
+          pricePrecision: number;
+          ratePrecision: number;
+        };
+        time: string;
+        highestBid: string;
+        lowestAsk: string;
+        rate: string;
+        previousRate: string;
+      };
+    }
+  | {
+      status: 'Fail';
+      errors: string[];
+    };
