41dad7989907db1c4cca860776364a84590f9ebc
Hubert <gielaktv@gmail.com>
2023-09-30 17:35:21 +0200

    feat: add button

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,21 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { useState } from 'react';
+
+import { Button } from './components/Button';
 
 function Hello() {
+  const [loading, setLoading] = useState(false);
   return (
     <div>
       <div className="w-36 h-36 bg-red-700">abc</div>
+      <Button
+        color="bg-red-500"
+        loading={loading}
+        onClick={() => setLoading(true)}
+      >
+        GENERUJ
+      </Button>
     </div>
   );
 }
