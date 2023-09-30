4751e39652039e876e40eb960c7f553f5cd803e3
Hubert <gielaktv@gmail.com>
2023-09-30 15:15:31 +0200

    fix: config

--- .erb/configs/webpack.config.renderer.prod.ts
+++ .erb/configs/webpack.config.renderer.prod.ts
@@ -39,6 +39,11 @@ const configuration: webpack.Configuration = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        include: [webpackPaths.srcRendererPath],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
         test: /\.s?(a|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
@@ -54,11 +59,11 @@ const configuration: webpack.Configuration = {
         ],
         include: /\.module\.s?(c|a)ss$/,
       },
-      {
-        test: /\.s?(a|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-        exclude: /\.module\.s?(c|a)ss$/,
-      },
+      // {
+      //   test: /\.s?(a|c)ss$/,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      //   exclude: /\.module\.s?(c|a)ss$/,
+      // },
       // Fonts
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
