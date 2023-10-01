52353861552e90b58174683a65f06a8885648ea5
Hubert <gielaktv@gmail.com>
2023-10-01 11:29:05 +0200

    fix: all

--- src/renderer/components/StepRaport.tsx
+++ src/renderer/components/StepRaport.tsx
@@ -33,10 +33,7 @@ export const StepRaport = () => {
   };
 
   return (
-    // <StateContext.Consumer>
-
     <div className="p-5 ">
-      {/* <pre>{`${JSON.stringify(cryptoObject)}`}</pre> */}
       <div className="p-5" id="PDF_Raport">
         <h1 className="text-3xl text-center">Raport {cryptoObject?.ID}</h1>
         <h1 className="text-3xl text-center">{cryptoObject?.raportName}</h1>
@@ -58,84 +55,111 @@ export const StepRaport = () => {
         </div>
         <div className="pt-10"></div>
         <table className=" text-[10px] mx-auto">
-          <tbody>
-            <tr className="text-center">
-              <th>Nazwa kryptoaktywa</th>
-              <th>Ilość kryptoaktywa</th>
-              <th>Kantor/GIełda</th>
-              <th>Link</th>
-              <th>Koszt za jedną kryptowalute</th>
-              <th>Suma</th>
-              <th>Czy przeliczono z USD?</th>
-              <th>Kurs USD NBP</th>
+          <tr className="text-center">
+            <th className="zero-margin">Nazwa kryptoaktywa</th>
+            <th className="zero-margin">Ilość kryptoaktywa</th>
+            <th className="zero-margin">Kantor/GIełda</th>
+            <th className="zero-margin">Link</th>
+            <th className="zero-margin">Koszt za jedną kryptowalute</th>
+            <th className="zero-margin">Suma</th>
+            <th className="zero-margin">Czy przeliczono z USD?</th>
+            <th className="zero-margin">Kurs USD NBP</th>
+          </tr>
+          {cryptoObject?.cryptos.map((crypto, index) => (
+            <tr key={index}>
+              <td className="border-r border-b border-solid border-black">
+                {crypto.name}
+              </td>
+              <td className="border-r border-b border-solid border-black">
+                {crypto.quantity}
+              </td>
+              <td className="border-r border-b border-solid border-black">
+                {crypto.exchangeRate.map((exchange, index) => (
+                  <tr>
+                    <td>{exchange.name}</td>
+                  </tr>
+                ))}
+              </td>
+              <td className="border-r border-b border-solid border-black">
+                {crypto.exchangeRate.map((exchange, index) => (
+                  <tr key={index}>
+                    <td>{exchange.link}</td>
+                  </tr>
+                ))}
+              </td>
+              <td className="border-r border-b border-solid border-black">
+                {crypto.exchangeRate.map((exchange, index) => (
+                  <tr key={index}>
+                    <td>
+                      {exchange.value
+                        ? exchange.currency === 'USD'
+                          ? (
+                              +(cryptoObject.averageNbpExchangeRate || 0) *
+                              +exchange.value
+                            ).toFixed(2) + ' PLN'
+                          : +exchange.value.toFixed(2) + ' PLN'
+                        : 'Brak'}
+                    </td>
+                  </tr>
+                ))}
+              </td>
+              <td className="border-r border-b border-solid border-black">
+                {crypto.exchangeRate.map((exchange, index) => (
+                  <tr key={index}>
+                    <td>
+                      {exchange.value
+                        ? exchange.currency === 'USD'
+                          ? (
+                              +(
+                                exchange.value *
+                                +(cryptoObject.averageNbpExchangeRate || 0)
+                              ) * +crypto.quantity
+                            ).toFixed(2) + ' PLN'
+                          : (+exchange.value * +crypto.quantity).toFixed(2) +
+                            ' PLN'
+                        : 'Brak'}
+                    </td>
+                  </tr>
+                ))}
+              </td>
+              <td className="border-r border-b border-solid border-black">
+                {crypto.exchangeRate.map((exchange, index) => (
+                  <tr key={index}>
+                    <td>{exchange.currency === 'USD' ? 'TAK' : 'NIE'}</td>
+                  </tr>
+                ))}
+              </td>
+              <td className="border-r border-b border-solid border-black">
+                {crypto.exchangeRate.map((exchange, index) => (
+                  <tr key={index}>
+                    <td>{cryptoObject.averageNbpExchangeRate}</td>
+                  </tr>
+                ))}
+              </td>
             </tr>
-          </tbody>
-          <tbody>
-            {cryptoObject?.cryptos.map((crypto, index) => (
-              <tr key={index}>
-                <td className="border-r border-b border-solid border-black">
-                  {crypto.name}
-                </td>
-                <td className="border-r border-b border-solid border-black">
-                  {crypto.quantity}
-                </td>
-                <td className="border-r border-b border-solid border-black">
-                  {crypto.exchangeRate.map((exchange, index) => (
-                    <tr>
-                      <td>{exchange.name}</td>
-                    </tr>
-                  ))}
-                </td>
-                <td className="border-r border-b border-solid border-black">
-                  {crypto.exchangeRate.map((exchange, index) => (
-                    <tr key={index}>
-                      <td>{exchange.link}</td>
-                    </tr>
-                  ))}
-                </td>
-                <td className="border-r border-b border-solid border-black">
-                  {crypto.exchangeRate.map((exchange, index) => (
-                    <tr key={index}>
-                      <td>
-                        {exchange.value ? exchange.value + 'PLN' : 'Brak'}
-                      </td>
-                    </tr>
-                  ))}
-                </td>
-                <td className="border-r border-b border-solid border-black">
-                  {crypto.exchangeRate.map((exchange, index) => (
-                    <tr key={index}>
-                      <td>
-                        {exchange.value
-                          ? exchange.value * crypto.quantity + 'PLN'
-                          : 'Brak'}
-                      </td>
-                    </tr>
-                  ))}
-                </td>
-                <td className="border-r border-b border-solid border-black">
-                  {crypto.exchangeRate.map((exchange, index) => (
-                    <tr key={index}>
-                      <td>TAK</td>
-                    </tr>
-                  ))}
-                </td>
-                <td className="border-r border-b border-solid border-black">
-                  {crypto.exchangeRate.map((exchange, index) => (
-                    <tr key={index}>
-                      <td>{cryptoObject.averageNbpExchangeRate}</td>
-                    </tr>
-                  ))}
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          ))}
         </table>
 
         <div className="pt-10"></div>
         <h1 className="text-3xl text-right">Podsumowanie</h1>
         <h2 className="text-2xl text-right">
-          Suma: {cryptoObject?.averagePrice} PLN
+          Suma:{' '}
+          {cryptoObject?.cryptos
+            .reduce((accumulator, currentValue) => {
+              const sum = currentValue.exchangeRate.reduce(
+                (acc, cur) =>
+                  acc +
+                  (cur.currency === 'USD' && cur.value
+                    ? currentValue.quantity *
+                      cur.value *
+                      (cryptoObject.averageNbpExchangeRate || 0)
+                    : currentValue.quantity * (cur.value || 0)),
+                0,
+              );
+              return accumulator + sum;
+            }, 0)
+            .toFixed(2)}{' '}
+          PLN
         </h2>
         <span className="text-xs">
           Szacunkową średnią wartość kryptoaktywa została oszacowana na
