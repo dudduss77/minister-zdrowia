37b66d368def4c852f97b27b07e775980ee1cfe0
Damian <damiankarbowiak17@gmail.com>
2023-10-01 07:02:31 +0200

    pdf

--- src/renderer/components/StepRaport.tsx
+++ src/renderer/components/StepRaport.tsx
@@ -1,27 +1,31 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { StateContext } from '../contexts/StateContext';
 import { Button } from './Button';
 import { createLog } from '../utils/createLog';
+import { generatePdf } from '../../utils/pdfService';
 
 export const StepRaport = () => {
-
+  const { cryptoObject, setCryptoObject } = useContext(StateContext);
   useEffect(() => {
     createLog({
       type: 'UPDATE_DATA',
-      data: {}
-    })
-  }, [])
-  
+      data: {},
+    });
+  }, []);
+
+  const handleClick = () => {
+    window.electron.ipcRenderer.sendMessage('generate-pdf', cryptoObject);
+  };
+
   return (
-    <StateContext.Consumer>
-      {({ cryptoObject, setCryptoObject }) => (
-        <div className="p-5">
-          <pre>{`${JSON.stringify(cryptoObject)}`}</pre>
-          <Button color="bg-red-500" onClick={() => console.log('pobierz')}>
-            POBIERZ PDF
-          </Button>
-        </div>
-      )}
-    </StateContext.Consumer>
+    // <StateContext.Consumer>
+
+    <div className="p-5">
+      <pre>{`${JSON.stringify(cryptoObject)}`}</pre>
+      <Button color="bg-red-500" onClick={handleClick}>
+        POBIERZ PDF
+      </Button>
+    </div>
+    // </StateContext.Consumer>
   );
 };
